a3e4ba19bf973186d0608dd1b0c471fa9dadf562
Hirusha Gamage <143500670+HirushaGamage10@users.noreply.github.com>
2024-11-30 21:09:28 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "h-1.jpeg",
+    min2: "h-10.jpeg",
+    min3: "h-4.jpeg",
+    min4: "h-8.jpeg",
+    min5: "h-9.jpeg",
+    min6: "h-3.jpeg",
+
+    max1: "h-3.jpeg",
+    max2: "h-10.jpeg",
+    max3: "h-1.jpeg",
+    max4: "h-9.jpeg",
+    max5: "h-4.jpeg",
+    max6: "h-5.jpeg", 
+}
